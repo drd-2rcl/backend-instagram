ba2a4a451d0bda3f52a0282b4de8c1a192311571
drd-2rcl <drd.rclrd@gmail.com>
2019-06-12 23:24:45 -0300

    was created method get and post

---
+++ src/routes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const multer = require('multer');
+const PostController = require('./controllers/post_controller');
+const uploadConfig = require('./config/upload');
+
+const routes = new express.Router();
+const upload = multer(uploadConfig);
+
+routes.get('/posts', PostController.index);
+routes.post('/posts', upload.single('image'), PostController.store);
+
+
+module.exports = routes;
