053c958cb52b8e54464a3f4f3625d8e89e2982e1
drd-2rcl <drd.rclrd@gmail.com>
2019-06-15 14:04:30 -0300

    was created router for functionality to likes

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const multer = require('multer');
 const PostController = require('./controllers/post_controller');
+const LikeController = require('./controllers/like_controller');
 const uploadConfig = require('./config/upload');
 
 const routes = new express.Router();
@@ -8,6 +9,6 @@ const upload = multer(uploadConfig);
 
 routes.get('/posts', PostController.index);
 routes.post('/posts', upload.single('image'), PostController.store);
-
+routes.post('/posts/:id/like', LikeController.store);
 
 module.exports = routes;
